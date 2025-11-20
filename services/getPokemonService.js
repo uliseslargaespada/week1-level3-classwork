b0425e8e01486ab5325367ab6976174dd7bfe8c2
Ulises Largaespada <ulisesedu7@gmail.com>
2025-11-19 19:09:13 -0600

    update structure of apiRequest functions

--- services/getPokemonService.js
+++ services/getPokemonService.js
@@ -1,18 +1,10 @@
 const baseUrl = 'https://pokeapi.co/api/v2';
 
-// Flow to make API requests
-/*
-  1. Create an async function to interact with the API using try and catch block
-  2. Call this function in the main stream of our app
-  3. We use the data of the API to render anything on scream
-*/
-/* Using Axios - Async Await */
-async function getPokemonAPI(pokemonName) {
-  const endpoint = `${baseUrl}/pokemon/${pokemonName}`;
-
+// Base function for API requests
+// A UTIL function
+async function makeAPIRequest(endpoint) {
   try {
     const response = await axios.get(endpoint);
-
     const data = await response.data;
 
     return {
@@ -20,54 +12,27 @@ async function getPokemonAPI(pokemonName) {
       data: data
     };
   } catch (error) {
-    
     return {
       success: false,
       error: error
     };
   }
+}
+
+// Specific functions using the base function
+async function getPokemonAPI(pokemonName) {
+  const endpoint = `${baseUrl}/pokemon/${pokemonName}`;
+  return makeAPIRequest(endpoint);
 }
 
 async function getAbilitiesAPI() {
   const endpoint = `${baseUrl}/ability`;
-
-  try {
-    const response = await axios.get(endpoint);
-
-    const data = await response.data;
-
-    return {
-      success: true,
-      data: data
-    };
-  } catch (error) {
-    
-    return {
-      success: false,
-      error: error
-    };
-  }
+  return makeAPIRequest(endpoint);
 }
 
 async function getItemAPI(itemId) {
   const endpoint = `${baseUrl}/item/${itemId}`;
-
-  try {
-    const response = await axios.get(endpoint);
-
-    const data = await response.data;
-
-    return {
-      success: true,
-      data: data
-    };
-  } catch (error) {
-    
-    return {
-      success: false,
-      error: error
-    };
-  }
+  return makeAPIRequest(endpoint);
 }
 
 export { getPokemonAPI, getAbilitiesAPI, getItemAPI };
